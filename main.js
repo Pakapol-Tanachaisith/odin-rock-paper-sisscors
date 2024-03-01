cbf127a436e6c149a5c1419257ea49fc4291ae4c
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2024-03-01 20:07:16 +0700

    Refactor code to works with the UI

--- main.js
+++ main.js
@@ -1,35 +1,80 @@
+const OPTIONS_MAP = {
+  ROCK: "rock",
+  PAPER: "paper",
+  SCISSORS: "scissors",
+};
+
+let playerScore = 0;
+let computerScore = 0;
+
+const playerScoreDisplay = document.querySelector("#player-score");
+const computerScoreDisplay = document.querySelector("#computer-score");
+
+function computerPick() {
+  const options = Object.values(OPTIONS_MAP);
+  return options[Math.floor(Math.random() * 3)];
+}
+
+function playerWin() {
+  playerScoreDisplay.textContent = playerScore + 1;
+  playerScore++;
+}
+
+function computerWin() {
+  computerScoreDisplay.textContent = computerScore + 1;
+  computerScore++;
+}
+
+function resetGame() {
+  alert(playerScore > computerScore ? "Player Win!" : "Computer Win!");
+
+  playerScore = 0;
+  computerScore = 0;
+
+  playerScoreDisplay.textContent = playerScore;
+  computerScoreDisplay.textContent = computerScore;
+}
+
+function playRound(playerChose) {
+  const computerChose = computerPick();
+  console.log({ playerChose, computerChose });
+
+  // Tie
+  if (playerChose === computerChose) return;
+
+  if (playerChose === OPTIONS_MAP.ROCK) {
+    if (computerChose === OPTIONS_MAP.SCISSORS) {
+      playerWin();
+    } else {
+      computerWin();
+    }
+  }
+
+  if (playerChose === OPTIONS_MAP.PAPER) {
+    if (computerChose === OPTIONS_MAP.ROCK) {
+      playerWin();
+    } else {
+      computerWin();
+    }
+  }
+
+  if (playerChose === OPTIONS_MAP.SCISSORS) {
+    if (computerChose === OPTIONS_MAP.PAPER) {
+      playerWin();
+    } else {
+      computerWin();
+    }
+  }
+
+  if (playerScore === 5 || computerScore === 5) {
+    console.log({ playerScore, computerScore });
+    resetGame();
+  }
+}
+
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const result = playRound(button.id, getComputerChoice());
-    console.log(result);
+    playRound(button.id);
   });
 });
-
-function getComputerChoice() {
-  // Generate random integer between 0 and 2
-  const randomInt = Math.floor(Math.random() * 3);
-  const choices = ["Rock", "Paper", "Scissors"];
-  return choices[randomInt];
-}
-
-function playRound(playerSelection, computerSelection) {
-  const playerChose = playerSelection.toLowerCase();
-  const computerChose = computerSelection.toLowerCase();
-
-  if (playerChose === computerChose) {
-    return "tie";
-  }
-
-  if (playerChose === "rock") {
-    return computerChose === "scissors" ? "win" : "lose";
-  }
-
-  if (playerChose === "paper") {
-    return computerChose === "rock" ? "win" : "lose";
-  }
-
-  if (playerChose === "scissors") {
-    return computerChose === "paper" ? "win" : "lose";
-  }
-}
