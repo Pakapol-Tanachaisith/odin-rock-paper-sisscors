790bf581f25254b80e73e2f0740d21e98d087fd6
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2024-03-01 19:05:55 +0700

    Build the UI

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const result = playRound(button.id, getComputerChoice());
+    console.log(result);
+  });
+});
+
 function getComputerChoice() {
   // Generate random integer between 0 and 2
   const randomInt = Math.floor(Math.random() * 3);
@@ -25,47 +33,3 @@ function playRound(playerSelection, computerSelection) {
     return computerChose === "paper" ? "win" : "lose";
   }
 }
-
-for (let i = 1; i >= 5; i++) {
-  console.log(i);
-}
-
-function playGame() {
-  console.log("playing game");
-  let playerScore = 0;
-  let computerScore = 0;
-
-  const textMap = {
-    win: "You win!",
-    tie: "It's a tie!",
-    lose: "You lose!",
-  };
-
-  for (let i = 1; i <= 5; i++) {
-    const userChose = prompt(`[Round ${i}] Rock, Paper, or Scissors?`);
-    const computerChose = getComputerChoice();
-    console.log({ userChose, computerChose });
-    const roundResult = playRound(userChose, computerChose);
-
-    if (roundResult === "win") {
-      playerScore++;
-    }
-    if (roundResult === "lose") {
-      computerScore++;
-    }
-
-    console.log(`Player: ${playerScore} | Computer: ${computerScore}`);
-  }
-
-  let gameResult;
-  if (playerScore > computerScore) {
-    gameResult = "win";
-  } else if (playerScore === computerScore) {
-    gameResult = "tie";
-  } else {
-    gameResult = "lose";
-  }
-
-  return alert(textMap[gameResult]);
-}
-playGame();
